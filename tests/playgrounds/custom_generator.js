052ee9856b87fb85c5285b4d670a1f3be244e58f
Jonathan Rubio <jon.rubio.92@gmail.com>
2021-12-29 22:13:02 -0500

    KAREL working in advanced_playground.html

---
+++ tests/playgrounds/custom_generator.js
@@ -0,0 +1,9 @@
+var KARELtoolbox = `
+<xml id="toolbox" style="display: none">
+<category name="New" colour="#134796">
+        <label text="Begin program" web-class="labelClass"></label>
+        <block type="prog_name"></block>
+    </category>
+</xml>
+`;
+
